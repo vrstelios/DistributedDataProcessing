6ccfdac9ce847800da2a83844e1b89d4c972f977
vrstelios <verstelios@gmail.com>
2024-06-22 19:23:44 +0300

    clearing codes

--- src/main.java
+++ src/main.java
@@ -6,9 +6,9 @@ import java.time.Duration;
 
 public class main {
 
-    private static final String REDIS_HOST = "192.168.2.2";
-    private static final int REDIS_PORT = 6379;
-    private static final String SQLITE_URL = "jdbc:sqlite:identifier.sqlite";
+    private static final String REDIS_HOST = "";
+    private static final int REDIS_PORT = ;
+    private static final String SQLITE_URL = "";
 
     public static void main(String[] args) throws Exception {
         try {
@@ -22,35 +22,35 @@ public class main {
 
             // call function for create, insert, pipelined hash join and  semi-join
             databases.sqlitedb.createSQLiteTable(sqliteConnection, "dataset1K");
-            //databases.sqlitedb.createSQLiteTable(sqliteConnection, "dataset100K");
-            //databases.sqlitedb.createSQLiteTable(sqliteConnection, "dataset250K");
-            //databases.sqlitedb.createSQLiteTable(sqliteConnection, "dataset500K");
-            //databases.sqlitedb.createSQLiteTable(sqliteConnection, "dataset1000K");
+            databases.sqlitedb.createSQLiteTable(sqliteConnection, "dataset100K");
+            databases.sqlitedb.createSQLiteTable(sqliteConnection, "dataset250K");
+            databases.sqlitedb.createSQLiteTable(sqliteConnection, "dataset500K");
+            databases.sqlitedb.createSQLiteTable(sqliteConnection, "dataset1000K");
 
             databases.sqlitedb.insertSQLiteData(sqliteConnection, "dataset1K", 1000);
-            //databases.sqlitedb.insertSQLiteData(sqliteConnection, "dataset100K", 100_000);
-            //databases.sqlitedb.insertSQLiteData(sqliteConnection, "dataset250K", 250_000);
-            //databases.sqlitedb.insertSQLiteData(sqliteConnection, "dataset500K", 500_000);
-            //databases.sqlitedb.insertSQLiteData(sqliteConnection, "dataset1000K", 1_000_000);
+            databases.sqlitedb.insertSQLiteData(sqliteConnection, "dataset100K", 100_000);
+            databases.sqlitedb.insertSQLiteData(sqliteConnection, "dataset250K", 250_000);
+            databases.sqlitedb.insertSQLiteData(sqliteConnection, "dataset500K", 500_000);
+            databases.sqlitedb.insertSQLiteData(sqliteConnection, "dataset1000K", 1_000_000);
 
             databases.redisdb.insertRedisData(jedis, "dataset1K", 1000);
-            //databases.redisdb.insertRedisData(jedis, "dataset100K", 100_000);
-            //databases.redisdb.insertRedisData(jedis, "dataset250K", 250_000);
-            //databases.redisdb.insertRedisData(jedis, "dataset500K", 500_000);
-            //databases.redisdb.insertRedisData(jedis, "dataset1000K", 1_000_000);
+            databases.redisdb.insertRedisData(jedis, "dataset100K", 100_000);
+            databases.redisdb.insertRedisData(jedis, "dataset250K", 250_000);
+            databases.redisdb.insertRedisData(jedis, "dataset500K", 500_000);
+            databases.redisdb.insertRedisData(jedis, "dataset1000K", 1_000_000);
 
             methods.pipelinedHashJoin(sqliteConnection, jedis, "dataset1K", Duration.ofHours(672));
-            //methods.pipelinedHashJoin(sqliteConnection, jedis, "dataset100K", Duration.ofHours(1000));
-            //methods.pipelinedHashJoin(sqliteConnection, jedis, "dataset250K", Duration.ofHours(1000));
-            //methods.pipelinedHashJoin(sqliteConnection, jedis, "dataset500K", Duration.ofHours(1000));
-            //methods.pipelinedHashJoin(sqliteConnection, jedis, "dataset1000K", Duration.ofHours(1000));
+            methods.pipelinedHashJoin(sqliteConnection, jedis, "dataset100K", Duration.ofHours(1000));
+            methods.pipelinedHashJoin(sqliteConnection, jedis, "dataset250K", Duration.ofHours(1000));
+            methods.pipelinedHashJoin(sqliteConnection, jedis, "dataset500K", Duration.ofHours(1000));
+            methods.pipelinedHashJoin(sqliteConnection, jedis, "dataset1000K", Duration.ofHours(1000));
 
 
             methods.semiJoin(sqliteConnection, jedis, "dataset1K");
-            //methods.semiJoin(sqliteConnection, jedis, "dataset100K");
-            //methods.semiJoin(sqliteConnection, jedis, "dataset250K");
-            //methods.semiJoin(sqliteConnection, jedis, "dataset500K");
-            //methods.semiJoin(sqliteConnection, jedis, "dataset1000K");
+            methods.semiJoin(sqliteConnection, jedis, "dataset100K");
+            methods.semiJoin(sqliteConnection, jedis, "dataset250K");
+            methods.semiJoin(sqliteConnection, jedis, "dataset500K");
+            methods.semiJoin(sqliteConnection, jedis, "dataset1000K");
 
             //databases.redisdb.displayRedisData(jedis);
 
